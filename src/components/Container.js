d5168b931212e44b47e95e6d835fef0f318e6de5
Oscar Bréhier <68658005+OscarBrehier@users.noreply.github.com>
2022-02-14 22:33:23 +0200

    🧶 | Metatags update

--- src/components/Container.js
+++ src/components/Container.js
@@ -13,26 +13,6 @@ const Container = (props) => {
     return (
 
         <>  
-            <Metatags>
-
-                <title>Oscar Bréhier</title>
-                <meta name="title" content={meta.title} />
-                <meta name="description" content={meta.description} />
-
-                <meta property="og:type" content="website" />
-                <meta property="og:url" content="https://brehieroscar.vercel.app" />
-                <meta property="og:title" content={meta.title} />
-                <meta property="og:description" content={meta.description} />
-                <meta property="og:image" content={meta.image} />
-
-                <meta property="twitter:card" content="summary_large_image" />
-                <meta property="twitter:url" content="https://brehieroscar.vercel.app/" />
-                <meta property="twitter:title" content={meta.title} />
-                <meta property="twitter:description" content={meta.description} />
-                <meta property="twitter:image" content={meta.image} />
-
-            </Metatags>
-
             <div>
                 {children}
             </div>
